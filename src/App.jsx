0910e06af5ab7059be474334947aa89b5be600de
PilarArroyave <mdelpilar.arroyave@udea.edu.co>
2023-05-16 14:58:55 -0500

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Home from './components/Home'
 
-
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <h2>Error 404 not found</h2>
+  }
+])
 
 function App() {
 
   return (
     <>
-      
+      <RouterProvider router = {router} />
     </>
   )
 }
--- src/helpers/Header.jsx
+++ src/helpers/Header.jsx
@@ -1,10 +1,10 @@
-
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+        
+    </header>
   )
 }
 
